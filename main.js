effecb20c0920c2d6a23058bfc375dbcf3d41567
JamesMoulang <jamesmoulang123@gmail.com>
2023-05-06 17:31:20 +0100

    electron functionality: full screen, exit, save, load, steam achievements

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
-const steamworks = require('steamworks.js');
-
-let steamworks_client;
+// managers
+const steam_manager = require('./steam_manager');
+const save_manager = require('./save_manager');
 
 function createWindow () {
   // Create the browser window.
@@ -16,41 +16,37 @@ function createWindow () {
       contextIsolation: false,
       nodeIntegration: true
     }
-  })
-
-  steamworks_client = steamworks.init(480);
-  console.log(steamworks_client.localplayer.getName());
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('index.html');
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  return mainWindow;
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  const mainWindow = createWindow();
+
+  // initialise steamworks - allows us to talk to steam API
+  steam_manager.init(mainWindow, ipcMain, 1031900);
+
+  console.log(`steam id is ${steam_manager.getSteamId().steamId64}.`);
+
+  // initialise save manager - allows us to save/load game files
+  save_manager.init(mainWindow, ipcMain, steam_manager.getSteamId().steamId64, 'kana_save.json');
 
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  // if (process.platform !== 'darwin') app.quit()
-})
-
-ipcMain.on('get_steamworks', (event) => {
-  event.returnValue = steamworks_client.localplayer.getName();
+  });
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('window-all-closed', function () {
+  // if (process.platform !== 'darwin') app.quit()
+
+    app.quit();
+});
